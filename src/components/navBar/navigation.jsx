57d41a69b845752e8ff9630cffa98373311aebd7
arsonite <tr_burak@hotmail.de>
2019-02-17 20:03:55 +0100

    still trying to fix url bug

--- src/components/navBar/navigation.jsx
+++ src/components/navBar/navigation.jsx
@@ -6,11 +6,9 @@ import { url } from '../../config.json';
 import './style/navigation.css';
 
 function Navigation(props) {
-  // TODO: Write a better, single line regex
-  let name = props.name.replace(/img\//g, '').replace(/.svg/g, '');
-  name = name.substring(0, 1).toUpperCase() + name.substring(1, name.length);
-
-  console.log(`${window.location.origin}/img/${name}_s.svg`);
+  const name =
+    props.name.substring(0, 1).toUpperCase() +
+    props.name.substring(1, props.name.length);
 
   return (
     <Link
@@ -19,7 +17,14 @@ function Navigation(props) {
       className={`navigation${props.selected ? ' current' : ''}`}
     >
       {props.isImage ? (
-        <img src={`${window.location.origin}/img/${name}_s.svg`} alt="" />
+        <img
+          src={
+            window.location.origin +
+            '/' +
+            (props.selected ? name.replace(/\.svg/g, '_s.svg') : name)
+          }
+          alt=""
+        />
       ) : (
         name
       )}
