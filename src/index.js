ec5efb4eb56c09d3227546d8f021f83e05a58c53
arsonite <tr_burak@hotmail.de>
2019-02-25 20:10:56 +0100

    prevent unauthorized insight into backend data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import APIEntry from './components/api/apiEntry';
 import PageEntry from './components/pageEntry/pageEntry';
+
+import authService from './services/authService';
 
 import * as serviceWorker from './services/serviceWorker';
 
@@ -12,6 +14,12 @@ import './style/index.css';
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
+      {window.location.pathname.includes('backend') &&
+      !authService.isUserLoggedIn() ? (
+        <Redirect to="/" />
+      ) : (
+        ''
+      )}
       <Route path={`/api`} render={props => <APIEntry {...props} />} />
       <Route path={`/`} render={props => <PageEntry {...props} />} />
     </Switch>
